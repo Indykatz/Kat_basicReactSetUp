da00465e731efa05c79123d8f765f04c3092649f
Indykatz <cats83@me.com>
2022-09-06 19:00:49 +0100

    demo notes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,26 @@
+// ContentItem component from component folder
 import ContentItem from "../components/contentItem";
+
+// Styles components from styles folder app.styles
 import { AppStyles, ContentStyled, Header1Styled } from "../styles/app.styles";
 
+// About page function
 const About = () => {
   return (
-    <AppStyles> 
+    // AppStyles styled component
+    <AppStyles>
+      {/* ContentStyled styled component */}
       <ContentStyled>
+        {/* Header1StyledComponent */}
         <Header1Styled>About</Header1Styled>
+        {/* ContentItem styled component */}
         <ContentItem />
+        {/* End of ContentStyled styled component */}
       </ContentStyled>
+      {/* End of AppStyles styled component */}
     </AppStyles>
   );
 };
 
+// export About used in App.js
 export default About;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,21 @@
+// Styled components from styles folder app.styles
 import { AppStyles, ContentStyled, Header1Styled } from "../styles/app.styles";
 
+// Home function
 const Home = () => {
   return (
+    // AppStyles styled component
     <AppStyles>
+      {/* ContentStyled styled component */}
       <ContentStyled>
-        <Header1Styled>About</Header1Styled>
+        {/* Header1StyledComponent */}
+        <Header1Styled>Home</Header1Styled>
+        {/* ContentItem styled component */}
       </ContentStyled>
+      {/* End of AppStyles styled component */}
     </AppStyles>
   );
 };
 
+// Export Home
 export default Home;
