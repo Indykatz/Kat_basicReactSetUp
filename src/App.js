eea54008721589b82e7cbd28bb2ed74a4dcf55ba
Indykatz <cats83@me.com>
2022-09-06 18:34:43 +0100

    demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import { AppStyles } from "./styles/app.styles";
+// Imports the react router dom functions
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Imports the style "AppStyles" from styles folder file app.styles.js
+// This is a styled component
+import { AppStyles } from "./styles/app.styles";
+
+// Imports the components Navbar, header and footer from components folder
 import Navbar from "./components/navbar";
-import Home from "./pages/home";
-import About from "./pages/about";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
+// Import pages Home and About from pages folder
+import Home from "./pages/home";
+import About from "./pages/about";
+
+// The App function that runs the whole up
 const App = () => {
   return (
+    // The style component `AppStyles`
     <AppStyles>
+      {/* BrowserRouter function - routes pages */}
       <BrowserRouter>
+        {/* Header Component */}
         <Header />
+        {/* Navbar Component */}
         <Navbar />
+        {/* Routes each page URL */}
         <Routes>
+          {/* Home page url */}
           <Route path="/" element={<Home />} />
+          {/* About page url */}
           <Route path="/about" element={<About />} />
+          {/* Clode Routes functions */}
         </Routes>
+        {/* Footer Component */}
         <Footer />
+        {/* End of BrowserRouter function */}
       </BrowserRouter>
+      {/* End of App styles component */}
     </AppStyles>
   );
 };
 
+// Exports the App function
 export default App;
